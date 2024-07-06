776e049806b2e970920ae4b7d219d12b415b6c21
Ton Nguyen <ngokko12a6@gmail.com>
2024-07-06 20:05:29 +0800

    Fix Heroku deployment issues

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'dist')));
